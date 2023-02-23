8ead7daad0105ec4d9bd39f99cdc3c3aac17938f
Donald Dominguez Jr <ddominguez3@nait.ca>
2023-02-23 02:10:19 -0700

    Creates the About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                </div>
+                <div></div>
+            </div>
+                <div className='max-w-[1000] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>
+                            Hi. I'm Donald, nice to meet you. Please take a look around.
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                        I am proficient in programming languages such as HTML, CSS, and JavaScript, which allows me to create visually appealing and interactive user interfaces that enhance the overall user experience. My deep understanding of user behavior and the psychology behind it enables me to design interfaces that are intuitive, easy to navigate, and visually stunning. In addition, my knowledge of various marketing techniques and platforms helps me create marketing campaigns that effectively target and engage specific audiences, ultimately driving revenue for my clients.
+                        </p>
+                    </div>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-yellow-500'>Hi my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Donald Jeoffrey</h1>
             <h2 className='text-4xl sm:text7xl font-bold text-[#8892b0]'>I'm a frontend Developer, UI/UX Designer, Marketing Strategist</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>As a frontend developer, UI-UX designer, and marketing strategist, I possess a unique blend of technical, creative, and analytical skills. I am proficient in programming languages such as HTML, CSS, and JavaScript, which allows me to create visually appealing and interactive user interfaces that enhance the overall user experience. My deep understanding of user behavior and the psychology behind it enables me to design interfaces that are intuitive, easy to navigate, and visually stunning. In addition, my knowledge of various marketing techniques and platforms helps me create marketing campaigns that effectively target and engage specific audiences, ultimately driving revenue for my clients.</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>As a frontend developer, UI-UX designer, and marketing strategist, I possess a unique blend of technical, creative, and analytical skills. </p>
         <div>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-gradient-to-r from-orange-600 via-yellow-500 to-yellow-300 hover:border-white'>
                 View Projects 

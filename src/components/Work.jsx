6342f3ca7187fa83307be8bc5da3d03aedd30343
Donald Dominguez Jr <ddominguez3@nait.ca>
2023-03-01 22:34:31 -0700

    adds work - community gallery, CSS-Grid, Explore

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import WorkImg from '../assets/projects/workImg.jpeg'
 import dashboard from '../assets/projects/dashboard.jpeg'
+import communityGalleryImg from '../assets/projects/communityGalleryImg.jpeg'
+import cssGridFLexbox from '../assets/projects/css-grid-flexbox.jpeg'
+import exploreElkPoint from '../assets/projects/exploreElkPoint.jpeg'
 import realEstate from '../assets/projects/realestate.jpg'
 import { Link } from 'react-router-dom'
 
@@ -47,22 +50,22 @@ const Work = () => {
                 </div>
 
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${realEstate})`}}  
+                <div style={{backgroundImage: `url(${communityGalleryImg})`}}  
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            PHP Community Gallery
                         </span>
                         <div>
                             <a href="/">
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Rationale</button>
                             </a>
-                            <a href="/">
+                            <a href="https://donaldjeoffrey.com/dmit/2503/multiuser-gallery/index.php" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/donalddjr/multiuser-gallery.git" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -70,22 +73,22 @@ const Work = () => {
                 </div>
 
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}}  
+                <div style={{backgroundImage: `url(${cssGridFLexbox})`}}  
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl text-center font-bold text-white tracking-wider'>
-                            React JS Application
+                            CSS Grid & Flexbox
                         </span>
                         <div>
                             <a href="/">
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Rationale</button>
                             </a>
-                            <a href="/">
+                            <a href="https://donaldjeoffrey.com/dmit/css-grid-lab/" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/donalddjr/css-grid-lab.git" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -93,22 +96,22 @@ const Work = () => {
                 </div>
 
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${realEstate})`}}  
+                <div style={{backgroundImage: `url(${exploreElkPoint})`}}  
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Explore Elk Point
                         </span>
                         <div>
-                        <a href="/">
+                            <a href="/">
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Rationale</button>
                             </a>
-                            <a href="/">
+                            <a href="https://donaldjeoffrey.com/dmit/1528/exploreelkpoint/" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/donalddjr/exploreelkpoint.git" target={'_blank'}>
                                 <button className='text-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
